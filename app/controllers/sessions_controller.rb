318b30950486d8a1cddad3a0b7e9a07594bc3e5b
AnkitaPal <ankitapal1225@gmail.com>
2018-08-31 18:19:12 -0400

    Landing Page and designing correction

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,11 @@
 class SessionsController < ApplicationController
+
   def new
   end
   def create
-  	user = User.find_by(email: params[:session][:email].downcase)
-  	if user and User.authenticate(params[:session][:email].downcase, params[:session][:password])
+  	#user = User.find_by(email: params[:session][:email].downcase)
+    user = User.authenticate(params[:session][:email].downcase, params[:session][:password])
+  	if user 
   		session[:user_id] = user.id
   		redirect_to '/dashboard', alert:"User Logged In"
   	else
@@ -13,6 +15,7 @@ class SessionsController < ApplicationController
 
   def destroy
   	session.delete(:user_id)
+    session.clear
     @current_user = nil
   	redirect_to "/login", alert:"Successfully logged out"
   	
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,16 @@
 class User < ApplicationRecord
-	attr_accessor :email, :password
+	attr_accessor :email,:password
 
 
 	def self.authenticate(email, password)
 		user = find_by_email(email)
-
+		#user = User.where(["email = ?", email]).first
+		#user = User.where("email = ? AND password = ?", email,  password)
+		#logger.debug user.id
+		logger.debug "email: "+user.email.to_s
+		#logger.debug user.password
 		return nil if user.nil?
-		return user if user.password?
+		#return user
+		return user if user.password==password
 	end
 end
