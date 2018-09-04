c86d16984d5f4adb56df8036cd579950a9364a1c
AnkitaPal <ankitapal1225@gmail.com>
2018-09-04 13:08:58 -0400

    Session created and user authentication

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,8 +4,8 @@ class SessionsController < ApplicationController
   end
   def create
   	user = User.find_by(email: params[:session][:email].downcase)
-    if user && User.authenticate(params[:session][:email], params[:session][:password])
-  		session[:user_id] = user.id
+    if user && user.authenticate(params[:session][:password])
+  		log_in user
   		redirect_to '/dashboard', alert:"User Logged In"
   	else
   		redirect_to "/login", alert:"Inavlid username or password"
@@ -13,9 +13,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-  	session.delete(:user_id)
-    session.clear
-    @current_user = nil
+  	log_out
   	redirect_to "/login", alert:"Successfully logged out"
   	
   end
